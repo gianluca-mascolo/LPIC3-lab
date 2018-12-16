dc127c8717245ff8493e070cae74e3b0b73862fe
Gianluca Mascolo <gianluca.mascolo@schibsted.com>
2018-12-16 17:12:40 +0100

    install xen in vagrant vm

--- xen_libvirt/scripts/install-xen.sh
+++ xen_libvirt/scripts/install-xen.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt-get -y install xen-linux-system-amd64 xen-tools xenstore-utils xen-hypervisor-4.4-amd64 xen-utils-4.4
-sed -ri 's/.*autoballoon.*/autoballon=0/' /etc/xen/xl.conf
-sed -ri 's/.*GRUB_CMDLINE_XEN.*//' /etc/default/grub
-echo 'GRUB_CMDLINE_XEN="dom0_mem=512M,max:512M"' >> /etc/default/grub
-update-grub
+sudo apt-get -y install curl wget apt-transport-https dirmngr
+sudo cp /tmp/sources.list /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install linux-image-4.9-amd64
+sudo apt-get -y install xen-linux-system-amd64 xen-tools xenstore-utils xen-hypervisor-4.4-amd64 xen-utils-4.4
+sudo sed -ri 's/.*autoballoon.*/autoballon=0/' /etc/xen/xl.conf
+sudo sed -ri 's/.*GRUB_CMDLINE_XEN.*//' /etc/default/grub
+sudo sed -ri 's/.*GRUB_CMDLINE_LINUX_DEFAULT.*//' /etc/default/grub
+sudo sed -ri 's/.*GRUB_TERMINAL.*//' /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=hvc0"' | sudo tee -a /etc/default/grub
+echo 'GRUB_TERMINAL="console"' | sudo tee -a /etc/default/grub
+echo 'GRUB_CMDLINE_XEN="dom0_mem=512M,max:512M console=com1,vga noreboot=true loglvl=all guest_loglvl=all"' | sudo tee -a /etc/default/grub
+sudo update-grub
